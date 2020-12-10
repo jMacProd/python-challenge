74a62d31175034744bc0f9687136d3a61b6d2811
Jay <j.sutton@vic.chariot.net.au>
2020-12-10 23:13:08 +1100

    Final Pybank submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,59 +14,38 @@ with open(csvpath, "r") as csvfile:
 # skip the first line as it contains the headers
     next(csvreader, None)
 
-    #header = next(csvreader)
-
-    #Total number of months = count the rows - Len
-    #create list of months
     for row in csvreader:
         
         #Total number of months = count the rows - Len
-            #create list of months
+        #create list of months
         monthlist.append(row[0])
         
         #net total amount of "Profit/Losses = progressive accumulation
-            #So create variable total = 0 and add to variable
+        #So create variable total = 0 and add to variable
         nettotal = nettotal + int(row[1])
 
         #Create list of value changes - don't include first cell
-            #list = (i.2 = (i.2 - 1))
-            #I think create a list of values then do item 2 -1
+        #Create a list of values then do (item 2 - 1)
         valuelist.append(row[1])
 
-        #Possibly Zip list of months and list of value changes?
-
-        # greatest increase in profits
-            # minimum value = min(List of value changes)
-            #how to identify the month it alligns to?
-
-        #greatest decrease in profits
-            # minimum value = max(List of value changes) 
-#rowcount = len(monthlist)
 
 #for change in valuelist - needs to be after for loop and no longer in open file :
-#    I was think this "valuechangelist.append(int(change[1])-int(change[1-1]))"
-#   but had to get from stackoveflow https://stackoverflow.com/questions/7172933/calculate-difference-between-adjacent-items-in-a-python-list
+#https://stackoverflow.com/questions/7172933/calculate-difference-between-adjacent-items-in-a-python-list
 valuelistdiff = [int(valuelist[n])-int(valuelist[n-1]) for n in range(1,len(valuelist))]
-#print (valuelistdiff)
-#print(len(valuelistdiff))
 
 #average of the changes in "Profit/Losses"
-# https://careerkarma.com/blog/python-average/#:~:text=We%20can%20find%20the%20average,the%20Python%20mean()%20function. 
-        #   "average = statistics.mean(List of value changes)"
+#https://careerkarma.com/blog/python-average/#:~:text=We%20can%20find%20the%20average,the%20Python%20mean()%20function. 
 average = statistics.mean(valuelistdiff)
-#print(round(average, 2))
 
-#create new month list removing the first item - not done
-#then create a dictionary of month:change - found how to convert existing 2 lists into dictionary
-    #https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+#Create a dictionary of month:change
+#https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+#Need to create not include first month on list
 monthchangedict = {monthlist[i+1]: valuelistdiff[i] for i in range(len(monthlist)-1)} 
 
 #find max value in dictionary and return key
-    #https://www.geeksforgeeks.org/python-get-key-with-maximum-value-in-dictionary/
+#https://www.geeksforgeeks.org/python-get-key-with-maximum-value-in-dictionary/
 greatestincreasekey = max(monthchangedict, key=monthchangedict.get) 
 greatestincreasevalue = max(valuelistdiff)
-#print(greatestincreasekey)
-#print(max(valuelistdiff))
 
 #find min value in dictinary and return key
 greatestdecreasekey = min(monthchangedict, key=monthchangedict.get) 
--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # python-challenge
 
+# Project Name
+> Here goes your awesome project description!
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+Add more general information about project. What the purpose of the project is? Motivation?
+
+## Screenshots
+![Example screenshot](./img/screenshot.png)
+
+## Technologies
+* Tech 1 - version 1.0
+* Tech 2 - version 2.0
+* Tech 3 - version 3.0
+
+## Setup
+Describe how to install / setup your local environement / add link to demo version.
+
+## Code Examples
+Show examples of usage:
+`put-your-code-here`
+
+## Features
+List of features ready and TODOs for future development
+* Awesome feature 1
+* Awesome feature 2
+* Awesome feature 3
+
+To-do list:
+* Wow improvement to be done 1
+* Wow improvement to be done 2
+
+## Status
+Project is: _in progress_, _finished_, _no longer continue_ and why?
+
+## Inspiration
+Add here credits. Project inspired by..., based on...
+
+## Contact
+Created by [@flynerdpl](https://www.flynerd.pl/) - feel free to contact me!
+
 
 voterlist = []
 candidatelist = []
@@ -9,9 +59,19 @@ clTest = []
 
 
 Resources
-    https://www.geeksforgeeks.org/python-get-unique-values-list/
-        To assist with identifying a complete list of candidates who received votes
 
-https://www.tutorialspoint.com/counting-the-frequencies-in-a-list-using-dictionary-in-python
+PyBank
+    https://stackoverflow.com/questions/7172933/calculate-difference-between-adjacent-items-in-a-python-list
+
+    https://careerkarma.com/blog/python-average/#:~:text=We%20can%20find%20the%20average,the%20Python%20mean()%20function. 
+
+    #https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+
+    https://www.geeksforgeeks.org/python-get-key-with-maximum-value-in-dictionary/
+
+PyPoll
+    https://www.geeksforgeeks.org/python-get-unique-values-list/
+
+    https://www.tutorialspoint.com/counting-the-frequencies-in-a-list-using-dictionary-in-python
 
 
