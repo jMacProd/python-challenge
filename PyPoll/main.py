4175fbe3157307814c490e9acbc08565638bbb99
Jay <j.sutton@vic.chariot.net.au>
2020-12-10 22:38:59 +1100

    Final submissin for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,14 +4,6 @@ import csv
 voterlist = []
 candidatelist = []
 clTest = []
-#uniquename = []
-
-
-#defining a function that can be used in the loop?
-#def candidate():
-#    for name in candidatelist:
-#        if name not in uniquename:
-#            uniquename.append(name)
 
 csvpath = os.path.join("Resources","election_data.csv")
 
@@ -23,36 +15,18 @@ with open(csvpath, "r") as csvfile:
         
     for row in csvreader:
 
-        #The total number of votes cast
-        #len(row)
+        #The full list of votes cast - allows for a len count
         voterlist.append(row[0])
 
+        #The full list of everytime a candidate is voted for - allows for a count of each candidate
         clTest.append(row[2])
 
-        #A complete list of candidates who received votes
+        #A unique list of candidates who received votes
         #https://www.geeksforgeeks.org/python-get-unique-values-list/
         if row[2] not in candidatelist: candidatelist.append(row[2])
 
-        
 
-   
-#        votecountdict = {}
-#        for name in candidatelist:
-#            votecountdict[name] = 1
-#                if name = row(2):
-#                    len(row2)
-#                else:
-#                    0)
-        
-            
-        #for candidate in candidatelist:
-            #if row[2] == candidate: khan = khan + 1
-            #votecountdict = {candidate:if row[2] = candidate: }
-         #   name + candidate = []
-          #  votecountdict = {candidate:(if row[2] == candidate):2}
-        
-
-
+#To tally each candidates' votes
 #https://www.tutorialspoint.com/counting-the-frequencies-in-a-list-using-dictionary-in-python
 votecountdict = {}
 for item in clTest:
@@ -60,48 +34,32 @@ for item in clTest:
         votecountdict[item] += 1
     else:
         votecountdict[item] = 1
-#for key, value in votecountdict.items():
-#    print("% s -> % d" % (key, value))
 
-
-#print(candidatelist) 
-#print(votecountdict)
- #   candidate()
- #   print(khan)
-
-
-#The percentage of votes each candidate won val
-
+#Create a list for the percentage of votes each candidate won
 percentage  = []
 
 for x in votecountdict:
     percentage.append(votecountdict[x] / len(voterlist))
 
-#print(percentage)
 
+#To call each value to print, they need to be in individual lists
 #split dictionary into list
-#prit both lists to check order
-#Print percentage list to check order
-#zip 3 lists
-#loop to print each value
+
+
 
 VCD_keys = votecountdict.keys() 
 VCD_values = votecountdict.values()
+
+#zip 3 lists and convert to list
 zipped = zip(VCD_keys, VCD_values, percentage)
 zipped_list = list(zipped)
-
-#for z in zipped_list:
-#   print(str(z[0]) + ": " + str('{:.3%}'. format(z[2])) + " (" + str(z[1]) + ")")
 
 
 #The winner of the election based on popular vote.
 winner = max(votecountdict, key=votecountdict.get) 
 
 
-
-#for key in votecountdict:
-#        for figure in percentage:
-#            print ("The key name is " + key + ": " + str('{0:.0%}'.format(figure)))
+#Need to create loop to allow each value to be printed separately in candidate tally summary
 
 print(f"Election Results")
 print(f"-------------------------")
